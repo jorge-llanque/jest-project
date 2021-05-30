e6b8c035702b2df451c70b27a14de0aa71dc452c
jorge <georgellanque@gmail.com>
2021-05-30 01:26:30 -0400

    Add maths.test.js

---
+++ __test__/maths.test.js
@@ -0,0 +1,16 @@
+import { sumar, restar, multiplicar, dividir } from "../maths";
+
+describe('Calculos matematicos', () => {
+    test('Prueba de sumas', () => {
+        expect(sumar(1,1)).toBe(2);
+    });
+    test('Prueba de resta', () => {
+        expect(restar(10, 5)).toBe(5);
+    });
+    test('Prueba de multiplicacion', () => {
+        expect(multiplicar(2,2)).toBe(4);
+    });
+    test('Prueba de division', () => {
+        expect(dividir(10,2)).toBe(5);
+    });
+});
