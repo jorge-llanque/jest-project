3eef07b168cce089383f025491d1a9a903cca67a
jorge <georgellanque@gmail.com>
2021-05-30 13:46:35 -0400

    Testing arrays

---
+++ __test__/arrays.test.js
@@ -0,0 +1,13 @@
+import { arrayColors, arrayFruits } from '../array';
+
+describe('Comprobaremos que existe un elemento', () => {
+  test('Tiene una banana', () => {
+    expect(arrayFruits()).toContain('banana');
+  });
+  test('No tiene platano', () => {
+    expect(arrayFruits()).not.toContain('PLATANO');
+  });
+  test('Comprobar el tamaño de un arreglo', () => {
+    expect(arrayFruits()).toHaveLength(6);
+  });
+});
