eb247bc12b11351b0dada555db8b8013f194fa51
jorge <georgellanque@gmail.com>
2021-06-02 16:16:25 -0400

    Testing snapshot exceptions

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -5,5 +5,23 @@ describe('Instantaneas', () => {
     test('Snapshot', () => {
         expect(getCharacter(rick)).toMatchSnapshot();
     })
+
+    test('Siempre fallara la instantanea', () => {
+        const user = {
+            createAt: new Date(),
+            id: Math.floor(Math.random() * 20)
+        }
+        expect(user).toMatchSnapshot();
+    })
+
+    test('Tenemos una excepcion dentro del codigo', () => {
+        const user = {
+            id: Math.floor(Math.random() * 20),
+            name: 'Jorge LLanque'
+        }
+        expect(user).toMatchSnapshot({
+            id: expect.any(Number)
+        })
+    })
     
 })
